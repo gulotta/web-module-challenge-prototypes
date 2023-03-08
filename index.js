1036ce058941d457584b75260e5f08a2a9069b2f
Michael Gulotta <gulottam@gmail.com>
2023-03-08 14:02:06 -0500

    all tasks complete

--- index.js
+++ index.js
@@ -15,10 +15,30 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+this.name = name;
+this.age = age;
+this.stomach = [];
 }
 
+Person.prototype.eat = function(edible){
+if(this.stomach.length < 10){
+  this.stomach.push(edible);
+}
+Person.prototype.poop = function (){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+}
+
+const mary = new Person ('Mary', 50);
+console.log(mary.toString());
+mary.eat('cheeseburger');
+console.log(mary.stomach);
+mary.poop();
+console.log(mary.stomach);
 
 /*
   TASK 2
@@ -36,10 +56,28 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+
+Car.prototype.drive = function(distance){
+  const driveMiles = this.tank * this.milesPerGallon;
+  if (distance <= driveMiles){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank = (distance / milesPerGallon);
+  } else {
+    this.odometer = this.odometer + driveMiles;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles`;
+  }
+}
 
 /*
   TASK 3
@@ -49,18 +87,27 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
 
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global Binding
+    - 'this' is within the window console
+  2. Implcit Binding
+    - object left of the '.' is 'this'
+  3. New Binding
+    - 'this' is the object created by constructor function
+  4. Explicit Binding
+    - whenever 'call' or 'apply' method is used, 'this' is defined
 */
 
 ///////// END OF CHALLENGE /////////
